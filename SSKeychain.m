5457710ba2bf103a1a5f74b84448d7012d64d6c4
Caleb Davenport <caleb@guicocoa.com>
2011-08-30 20:13:03 -0400

    minor code cleanup

--- SSKeychain.m
+++ SSKeychain.m
@@ -30,31 +30,24 @@ NSString *SSKeychainErrorDomain = @"com.samsoffes.sskeychain";
 	return [self passwordForService:service account:account error:nil];
 }
 + (NSString *)passwordForService:(NSString *)service account:(NSString *)account error:(NSError **)error {
-	OSStatus status = SSKeychainErrorBadArguments;
+    OSStatus status = SSKeychainErrorBadArguments;
 	NSString *result = nil;
-	
-	if (0 < [service length] && 0 < [account length]) {
-		CFDataRef passwordData = NULL;
+	if (service && service) {
+		CFDataRef data = NULL;
 		NSMutableDictionary *query = [self queryForService:service account:account];
 		[query setObject:(id)kCFBooleanTrue forKey:(id)kSecReturnData];
 		[query setObject:(id)kSecMatchLimitOne forKey:(id)kSecMatchLimit];
-		
-		status = SecItemCopyMatching((CFDictionaryRef)query,
-											  (CFTypeRef *)&passwordData);
-		if (status == noErr && 0 < [(NSData *)passwordData length]) {
-			result = [[[NSString alloc] initWithData:(NSData *)passwordData
-											encoding:NSUTF8StringEncoding] autorelease];
+		status = SecItemCopyMatching((CFDictionaryRef)query, (CFTypeRef *)&data);
+		if (status == noErr && CFDataGetLength(data) > 0) {
+			result = [[[NSString alloc] initWithData:(NSData *)data encoding:NSUTF8StringEncoding] autorelease];
 		}
-		
-		if (passwordData != NULL) {
-			CFRelease(passwordData);
+		if (data != NULL) {
+			CFRelease(data);
 		}
 	}
-	
 	if (status != noErr && error != NULL) {
 		*error = [NSError errorWithDomain:SSKeychainErrorDomain code:status userInfo:nil];
 	}
-	
 	return result;
 }
 
@@ -64,16 +57,15 @@ NSString *SSKeychainErrorDomain = @"com.samsoffes.sskeychain";
 }
 + (BOOL)deletePasswordForService:(NSString *)service account:(NSString *)account error:(NSError **)error {
 	OSStatus status = SSKeychainErrorBadArguments;
-	if (0 < [service length] && 0 < [account length]) {
+	if (service && account) {
 		NSMutableDictionary *query = [self queryForService:service account:account];
 		status = SecItemDelete((CFDictionaryRef)query);
 	}
-	
 	if (status != noErr && error != NULL) {
 		*error = [NSError errorWithDomain:SSKeychainErrorDomain code:status userInfo:nil];
 	}
-	
-	return status == noErr;
+	return (status == noErr);
+    
 }
 
 #pragma mark - set passwords
@@ -82,20 +74,16 @@ NSString *SSKeychainErrorDomain = @"com.samsoffes.sskeychain";
 }
 + (BOOL)setPassword:(NSString *)password forService:(NSString *)service account:(NSString *)account error:(NSError **)error {
 	OSStatus status = SSKeychainErrorBadArguments;
-	if (0 < [service length] && 0 < [account length]) {
-		[self deletePasswordForService:service account:account];
-		if (0 < [password length]) {
-			NSMutableDictionary *query = [self queryForService:service account:account];
-			NSData *passwordData = [password dataUsingEncoding:NSUTF8StringEncoding];
-			[query setObject:passwordData forKey:(id)kSecValueData];
-			status = SecItemAdd((CFDictionaryRef)query, NULL);
-		}
+	if (password && service && account) {
+        [self deletePasswordForService:service account:account];
+        NSMutableDictionary *query = [self queryForService:service account:account];
+        NSData *data = [password dataUsingEncoding:NSUTF8StringEncoding];
+        [query setObject:data forKey:(id)kSecValueData];
+        status = SecItemAdd((CFDictionaryRef)query, NULL);
 	}
-	
 	if (status != noErr && error != NULL) {
 		*error = [NSError errorWithDomain:SSKeychainErrorDomain code:status userInfo:nil];
 	}
-	
 	return status == noErr;
 }
 
