b41c2a495fef35f52b4b1fbada8efd0f72d4d1c1
Sam Soffes <sam@samsoff.es>
2011-10-03 22:09:30 -0700

    Added basic tests

---
+++ Tests/SSKeychainTests.h
@@ -0,0 +1,13 @@
+//
+//  SSKeychainTests.h
+//  SSKeychainTests
+//
+//  Created by Sam Soffes on 10/3/11.
+//  Copyright (c) 2011 Sam Soffes. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SSKeychainTests : SenTestCase
+
+@end
---
+++ Tests/SSKeychainTests.m
@@ -0,0 +1,51 @@
+//
+//  SSKeychainTests.m
+//  SSKeychainTests
+//
+//  Created by Sam Soffes on 10/3/11.
+//  Copyright (c) 2011 Sam Soffes. All rights reserved.
+//
+
+#import "SSKeychainTests.h"
+#import "SSKeychain.h"
+
+static NSString *kSSToolkitTestsServiceName = @"SSToolkitTestService";
+static NSString *kSSToolkitTestsAccountName = @"SSToolkitTestAccount";
+static NSString *kSSToolkitTestsPassword = @"SSToolkitTestPassword";
+
+@interface SSKeychainTests ()
+- (BOOL)_accounts:(NSArray *)accounts containsAccountWithName:(NSString *)name;
+@end
+
+@implementation SSKeychainTests
+
+- (void)testAll {
+	// Getting & Setings Passwords
+	[SSKeychain setPassword:kSSToolkitTestsPassword forService:kSSToolkitTestsServiceName account:kSSToolkitTestsAccountName];
+	NSString *password = [SSKeychain passwordForService:kSSToolkitTestsServiceName account:kSSToolkitTestsAccountName];
+	STAssertEqualObjects(password, kSSToolkitTestsPassword, @"Password reads and writes");
+	
+	// Getting Accounts
+	NSArray *accounts = [SSKeychain accounts];
+	STAssertTrue([self _accounts:accounts containsAccountWithName:kSSToolkitTestsAccountName], @"All accounts");
+
+	accounts = [SSKeychain accountsForService:kSSToolkitTestsServiceName];
+	STAssertTrue([self _accounts:accounts containsAccountWithName:kSSToolkitTestsAccountName], @"Account for service");
+	
+	// Deleting Passwords
+	[SSKeychain deletePasswordForService:kSSToolkitTestsServiceName account:kSSToolkitTestsAccountName];
+	password = [SSKeychain passwordForService:kSSToolkitTestsServiceName account:kSSToolkitTestsAccountName];
+	STAssertNil(password, @"Password deletes");
+}
+
+
+- (BOOL)_accounts:(NSArray *)accounts containsAccountWithName:(NSString *)name {
+	for (NSDictionary *dictionary in accounts) {
+		if ([[dictionary objectForKey:@"acct"] isEqualToString:name]) {
+			return YES;
+		}
+	}
+	return NO;
+}
+
+@end
