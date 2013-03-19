95545fdc5833943ccdfa6b0a666d46b182c57421
Caleb Davenport <caleb@seesaw.co>
2013-03-19 17:46:32 -0400

    Move keychain query into its own class filegs

--- SSKeychain/SSKeychain.h
+++ SSKeychain/SSKeychain.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2009-2011 Sam Soffes. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "SSKeychainQuery.h"
 
 /**
  Error code specific to SSKeychain that can be returned in NSError objects.
@@ -164,70 +164,4 @@ extern NSString *const kSSKeychainWhereKey;
 
 @end
 
-/**
- Simple interface for querying or modifying keychain items.
- */
-@interface SSKeychainQuery : NSObject
 
-/** kSecAttrAccount */
-@property (nonatomic, copy) NSString *account;
-
-/** kSecAttrService */
-@property (nonatomic, copy) NSString *service;
-
-/** kSecAttrLabel */
-@property (nonatomic, copy) NSString *label;
-
-/** kSecAttrAccessGroup (only used on iOS) */
-@property (nonatomic, copy) NSString *accessGroup;
-
-/**
- You do not need to set both of these.
- */
-@property (nonatomic, copy) NSString *password;
-@property (nonatomic, copy) NSData *passwordData;
-
-/**
- Save the receiver's attributes as a keychain item. Existing items with the
- given account, service, and access group will first be deleted.
- 
- @param error Populated should an error occur.
- 
- @return `YES` if saving was successful, `NO` otherwise.
- */
-- (BOOL)save:(NSError **)error;
-
-/**
- Dete keychain items that match the given account, service, and access group.
- 
- @param error Populated should an error occur.
- 
- @return `YES` if saving was successful, `NO` otherwise.
- */
-- (BOOL)delete:(NSError **)error;
-
-/**
- Fetch all keychain items that match the given account, service, and access
- group. The values of `password` and `passwordData` are ignored when fetching.
- 
- @param error Populated should an error occur.
- 
- @return An array of dictionaries that represent all matching keychain items or
- `nil` should an error occur.
- The order of the items is not determined.
- */
-- (NSArray *)fetchAll:(NSError **)error;
-
-/**
- Fetch the keychain item that matches the given account, service, and access
- group. The `password` and `passwordData` properties will be populated unless
- an error occurs. The values of `password` and `passwordData` are ignored when
- fetching.
- 
- @param error Populated should an error occur.
- 
- @return `YES` if fetching was successful, `NO` otherwise.
- */
-- (BOOL)fetch:(NSError **)error;
-
-@end
