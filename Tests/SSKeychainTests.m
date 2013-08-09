95ace524ccc0440cbf71edda431776ab58bde087
Christopher Loessl <cloessl@x-berg.de>
2013-08-09 20:27:31 +0200

    Test cases added
    Code Coverage:
    86.7% SSKeychainQuery.m
    100 % SSKeychain.m

--- Tests/SSKeychainTests.m
+++ Tests/SSKeychainTests.m
@@ -13,6 +13,7 @@
 static NSString *kSSToolkitTestsServiceName = @"SSToolkitTestService";
 static NSString *kSSToolkitTestsAccountName = @"SSToolkitTestAccount";
 static NSString *kSSToolkitTestsPassword = @"SSToolkitTestPassword";
+static NSString *kSSToolkitTestsLabel = @"SSToolkitLabel";
 
 @interface SSKeychainTests : SenTestCase
 
@@ -32,6 +33,7 @@ static NSString *kSSToolkitTestsPassword = @"SSToolkitTestPassword";
     query.password = kSSToolkitTestsPassword;
     query.service = kSSToolkitTestsServiceName;
     query.account = kSSToolkitTestsAccountName;
+    query.label = kSSToolkitTestsLabel;
     STAssertTrue([query save:&error], @"Unable to save item: %@", error);
     
     // check password
@@ -73,8 +75,70 @@ static NSString *kSSToolkitTestsPassword = @"SSToolkitTestPassword";
     query.service = kSSToolkitTestsServiceName;
     query.account = kSSToolkitTestsAccountName;
     STAssertTrue([query deleteItem:&error], @"Unable to delete password: %@", error);
+    
+    // check if saving with missing informations is handled correctly
+    query = [[SSKeychainQuery alloc] init];
+    query.service = kSSToolkitTestsServiceName;
+    query.account = kSSToolkitTestsAccountName;
+    STAssertFalse([query save:&error], @"Function should return NO as not all needed information is provided: %@", error);
+    
+    query = [[SSKeychainQuery alloc] init];
+    query.password = kSSToolkitTestsPassword;
+    query.account = kSSToolkitTestsAccountName;
+    STAssertFalse([query save:&error], @"Function should return NO as not all needed information is provided: %@", error);
+
+    query = [[SSKeychainQuery alloc] init];
+    query.password = kSSToolkitTestsPassword;
+    query.service = kSSToolkitTestsServiceName;
+    STAssertFalse([query save:&error], @"Function save should return NO if not all needed information is provided: %@", error);
+    
+    // check if deletion with missing information is handled correctly
+    query = [[SSKeychainQuery alloc] init];
+    query.account = kSSToolkitTestsAccountName;
+    STAssertFalse([query deleteItem:&error], @"Function deleteItem should return NO if not all needed information is provided: %@", error);
+
+    query = [[SSKeychainQuery alloc] init];
+    query.service = kSSToolkitTestsServiceName;
+    STAssertFalse([query deleteItem:&error], @"Function deleteItem should return NO if not all needed information is provided: %@", error);
+    
+    // check if fetch handels missing information correctly
+    query = [[SSKeychainQuery alloc] init];
+    query.account = kSSToolkitTestsAccountName;
+    STAssertFalse([query fetch:&error], @"Function fetch should return NO if not all needed information is provided: %@", error);
+    
+    query = [[SSKeychainQuery alloc] init];
+    query.service = kSSToolkitTestsServiceName;
+    STAssertFalse([query fetch:&error], @"Function fetch should return NO if not all needed information is provided: %@", error);
 }
 
+- (void)testSSKeychain {
+    NSError *error = nil;
+    
+    // Test Class Methods of SSKeychain
+    
+    // create a new keychain item
+    STAssertTrue([SSKeychain setPassword:kSSToolkitTestsPassword forService:kSSToolkitTestsServiceName account:kSSToolkitTestsAccountName error:&error], @"Unable to save item: %@", error);
+    
+    // check password
+    STAssertEqualObjects([SSKeychain passwordForService:kSSToolkitTestsServiceName account:kSSToolkitTestsAccountName], kSSToolkitTestsPassword, @"Passwords were not equal");
+    
+    // check all accounts
+    STAssertTrue([self _accounts:[SSKeychain allAccounts] containsAccountWithName:kSSToolkitTestsAccountName], @"Matching account was not returned");
+    // check account
+    STAssertTrue([self _accounts:[SSKeychain accountsForService:kSSToolkitTestsServiceName] containsAccountWithName:kSSToolkitTestsAccountName], @"Matching account was not returned");
+    
+    // delete password
+    STAssertTrue([SSKeychain deletePasswordForService:kSSToolkitTestsServiceName account:kSSToolkitTestsAccountName error:&error], @"Unable to delete password: %@", error);
+    
+    // set password and delete it without error function
+    STAssertTrue([SSKeychain setPassword:kSSToolkitTestsPassword forService:kSSToolkitTestsServiceName account:kSSToolkitTestsAccountName], @"Unable to save item");
+    STAssertTrue([SSKeychain deletePasswordForService:kSSToolkitTestsServiceName account:kSSToolkitTestsAccountName], @"Unable to delete password");
+    
+#if __IPHONE_4_0 && TARGET_OS_IPHONE
+    [SSKeychain setAccessibilityType:kSecAttrAccessibleWhenUnlockedThisDeviceOnly];
+    STAssertTrue([SSKeychain accessibilityType] == kSecAttrAccessibleWhenUnlockedThisDeviceOnly, @"Unable to verify accessibilityType");
+#endif
+}
 
 - (BOOL)_accounts:(NSArray *)accounts containsAccountWithName:(NSString *)name {
 	for (NSDictionary *dictionary in accounts) {
